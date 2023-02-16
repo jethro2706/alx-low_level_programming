8b5bcf0bf6baaac63e2a31bad65c5f27e3d0a453
Raidon Naidoo <raidonj27@gmail.com>
2023-02-16 11:28:54 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(void)
 {
-   fprintf(stderr, "with proper grammar, but the outcome is a piece of art,");
-   getchar();
-   return 0;
+fprintf(stderr, 'Anything')
+fprintf("with proper grammar, but the outcome is a piece of art,");
+getchar();
+return 0;
 }
