da02daa5ecd5348f1378a2632f643920068137fc
Raidon Naidoo <raidonj27@gmail.com>
2023-02-20 09:37:05 -0800

    az

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,10 +4,9 @@ int main(void)
 {
 	/*declare a variable*/
 	char alphabet;
-
 	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
 	{
-	/*print the value of the variable*/ 
+	/*print the value of the variable*/
 	putchar(alphabet);
 	}
 	putchar('\n');
