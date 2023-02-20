c19e883faa23ccb95f511f98b5da54e85c61d35c
Raidon Naidoo <raidonj27@gmail.com>
2023-02-20 13:11:38 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * Description: 'the program's description'
- *
- * Return: Always (-1) (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	getchar();
-	return (-1);
+	return (0);
 }
-
