d7db424814527c3c92738e96cd3efbb642e44b8c
Raidon Naidoo <raidonj27@gmail.com>
2023-02-20 04:50:01 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-int main()
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always (-1) (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	getchar();
-	return 0;
+	return (-1);
 }
 
